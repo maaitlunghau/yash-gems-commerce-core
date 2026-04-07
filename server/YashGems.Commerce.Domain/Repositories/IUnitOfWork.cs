3b9e99af5c0279d6b00e297b5d38499b059b1b37
maaitlunghau <chunhau.py@gmail.com>
2026-04-07 15:19:32 +0700

    feat(domain): update unit of work interface for full transaction

--- server/YashGems.Commerce.Domain/Repositories/IUnitOfWork.cs
+++ server/YashGems.Commerce.Domain/Repositories/IUnitOfWork.cs
@@ -3,4 +3,7 @@ namespace YashGems.Commerce.Domain.Repositories;
 public interface IUnitOfWork : IDisposable
 {
     Task<int> CompleteAsync();
+    Task BeginTransactionAsync();
+    Task CommitTransactionAsync();
+    Task RollbackTransactionAsync();
 }
