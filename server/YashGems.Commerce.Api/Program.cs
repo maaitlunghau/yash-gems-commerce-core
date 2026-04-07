157274166c2580d3ca90b18ca96ca6df708a0302
maaitlunghau <chunhau.py@gmail.com>
2026-04-07 17:27:45 +0700

    feat(api): add seed data for catalog

--- server/YashGems.Commerce.Api/Program.cs
+++ server/YashGems.Commerce.Api/Program.cs
@@ -46,6 +46,8 @@ if (app.Environment.IsDevelopment())
 
         await db.Database.EnsureDeletedAsync();
         await db.Database.EnsureCreatedAsync();
+
+        await DbInitializer.SeedAsync(db);
     }
 }
 
