7c345bf919e72c5d90df7440b3046a843e01984c
maaitlunghau <chunhau.py@gmail.com>
2026-04-07 15:04:46 +0700

    feat(application): add certification service interface

---
+++ server/YashGems.Commerce.Application/Interfaces/ICertificationService.cs
@@ -0,0 +1,12 @@
+using YashGems.Commerce.Application.DTOs;
+
+namespace YashGems.Commerce.Application.Interfaces;
+
+public interface ICertificationService
+{
+    Task<IEnumerable<CertificationDto>> GetAllAsync();
+    Task<CertificationDto?> GetByIdAsync(int id);
+    Task CreateAsync(CertificationDto dto);
+    Task UpdateAsync(int id, CertificationDto dto);
+    Task DeleteAsync(int id);
+}
