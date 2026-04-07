0ce653fb4df129d614615cd7eff6678f5f2517de
maaitlunghau <chunhau.py@gmail.com>
2026-04-07 14:55:56 +0700

    feat(domain): update Certification entity

--- server/YashGems.Commerce.Domain/Entities/Certification.cs
+++ server/YashGems.Commerce.Domain/Entities/Certification.cs
@@ -1,8 +1,8 @@
-using System;
-
 namespace YashGems.Commerce.Domain.Entities;
 
 public class Certification
 {
-
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public bool IsActive { get; set; } = true;
 }
