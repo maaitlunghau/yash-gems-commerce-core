65f57334bff7f6aa59fc587e4b5a7a59e85eb2c1
maaitlunghau <chunhau.py@gmail.com>
2026-04-07 16:17:26 +0700

    feat(api): register DI for jewel type repository

--- server/YashGems.Commerce.Api/Program.cs
+++ server/YashGems.Commerce.Api/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IJewelTypeRepository, JewelTypeRepository>();
 builder.Services.AddScoped<IBrandRepository, BrandRepository>();
 builder.Services.AddScoped<IGoldKaratRepository, GoldKaratRepository>();
 builder.Services.AddScoped<IGoldKaratRepository, GoldKaratRepository>();
