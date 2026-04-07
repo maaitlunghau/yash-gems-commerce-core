f9d832238acd96a0f20cc10b297a5f7a326411cd
maaitlunghau <chunhau.py@gmail.com>
2026-04-07 14:59:42 +0700

    feat(application): configure mapping profile for auto mapper

--- server/YashGems.Commerce.Application/Mappings/MappingProfile.cs
+++ server/YashGems.Commerce.Application/Mappings/MappingProfile.cs
@@ -1,3 +1,4 @@
+using YashGems.Commerce.Application.DTOs;
 using YashGems.Commerce.Domain.Entities;
 
 namespace YashGems.Commerce.Application.Mappings;
@@ -6,6 +7,18 @@ public class MappingProfile : AutoMapper.Profile
 {
     public MappingProfile()
     {
-        // CreateMap<Category, CategoryDto>();
+        // entity -> dto
+        CreateMap<Category, CategoryDto>();
+        CreateMap<JewelType, JewelTypeDto>();
+        CreateMap<Brand, BrandDto>();
+        CreateMap<GoldKarat, GoldKaratDto>();
+        CreateMap<Certification, CertificationDto>();
+
+        // dto -> entity
+        CreateMap<CategoryDto, Category>();
+        CreateMap<JewelTypeDto, JewelType>();
+        CreateMap<BrandDto, Brand>();
+        CreateMap<GoldKaratDto, GoldKarat>();
+        CreateMap<CertificationDto, Certification>();
     }
 }
