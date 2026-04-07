9df9f7a2ee5ff604e71f2114067c62c2a36a711a
maaitlunghau <chunhau.py@gmail.com>
2026-04-07 15:04:23 +0700

    feat(application): add brand service interface

---
+++ server/YashGems.Commerce.Application/Interfaces/IBrandService.cs
@@ -0,0 +1,12 @@
+using YashGems.Commerce.Application.DTOs;
+
+namespace YashGems.Commerce.Application.Interfaces;
+
+public interface IBrandService
+{
+    Task<IEnumerable<BrandDto>> GetAllAsync();
+    Task<BrandDto?> GetByIdAsync(int id);
+    Task CreateAsync(BrandDto dto);
+    Task UpdateAsync(int id, BrandDto dto);
+    Task DeleteAsync(int id);
+}
