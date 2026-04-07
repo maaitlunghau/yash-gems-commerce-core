84ef28bd8fcf1989cbc0b1f64811d6606c43e336
maaitlunghau <chunhau.py@gmail.com>
2026-04-07 14:51:20 +0700

    chore(application): install AutoMapper package

--- server/YashGems.Commerce.Application/obj/Debug/net8.0/YashGems.Commerce.Application.AssemblyInfo.cs
+++ server/YashGems.Commerce.Application/obj/Debug/net8.0/YashGems.Commerce.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YashGems.Commerce.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e74c0c257b5c48ba665ffef414dd98a2daa03421")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ada151cf80b06d607dd643c9ba21cbfa2a0172")]
 [assembly: System.Reflection.AssemblyProductAttribute("YashGems.Commerce.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YashGems.Commerce.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
